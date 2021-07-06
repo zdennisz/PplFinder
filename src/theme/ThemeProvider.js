6152fa376b26fcbcd4473316810794103ac93184
Omer Bresinski <omer@powerlink.co.il>
2021-07-06 17:43:18 +0300

    Scrollbar

--- src/theme/ThemeProvider.js
+++ src/theme/ThemeProvider.js
@@ -1,33 +1,30 @@
 import React from "react";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { createMuiTheme, ThemeProvider as Provider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 const ThemeProvider = ({ children }) => {
-    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: "dark",
+          primary: {
+            main: "#84ffff",
+          },
+          error: {
+            main: "#f06292",
+          },
+        },
+      }),
+    []
+  );
 
-    const theme = React.useMemo(
-        () =>
-            createMuiTheme({
-                palette: {
-                    type: prefersDarkMode ? "dark" : "light",
-                    primary: {
-                        main: "#84ffff",
-                    },
-                    error: {
-                        main: "#f06292",
-                    },
-                },
-            }),
-        [prefersDarkMode]
-    );
-
-    return (
-        <Provider theme={theme}>
-            <CssBaseline />
-            {children}
-        </Provider>
-    );
+  return (
+    <Provider theme={theme}>
+      <CssBaseline />
+      {children}
+    </Provider>
+  );
 };
 
 export default ThemeProvider;
