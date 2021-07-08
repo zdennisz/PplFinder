00768a9e5b15acf74f4f5716ae0ef966be689959
Omer Bresinski <omer@powerlink.co.il>
2021-07-08 16:22:27 +0300

    Removed comment

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -22,30 +22,3 @@ const Home = () => {
 };
 
 export default Home;
-
-//Change Filters UI (Categories)
-
-// Task
-// Automatically loads the first 25 records
-//The applicant needs to:
-
-//1: -- Difficulty I
-////Add filters by:
-//////Gender
-//////Nationalities: [BR, AU]
-
-//2:
-////Favorite: -- Difficulty II
-//////Implement the favorite button click functionality
-////////////Favorite users always show the heart icon
-////////////Favorite users get saved into local storage
-//////Implement a favorite users page which loads all favorites
-////////////Clicking on the favorite button for the second time
-////////////causes an "un-favorite" and removes from the favorite page
-
-//3:
-////Add infinity scroll -- Difficulty III
-////Live Interview - Virtualization
-
-//4:
-////Implement routing logic on top of existing UI implementation
